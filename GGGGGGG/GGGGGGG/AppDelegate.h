cdc9ed9bf15e085d22c8ea8e00cec638bf254393
zhnagbaozhong <1083154279@qq.com>
2016-03-17 09:39:35 +0800

    第一次提交

---
+++ GGGGGGG/GGGGGGG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GGGGGGG
+//
+//  Created by lanou3g on 16/3/17.
+//  Copyright © 2016年 蓝欧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GGGGGGG/GGGGGGG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GGGGGGG
+//
+//  Created by lanou3g on 16/3/17.
+//  Copyright © 2016年 蓝欧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
